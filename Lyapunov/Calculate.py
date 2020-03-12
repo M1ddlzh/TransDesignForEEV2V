4462dac311358f49d07ca58b211869ee8917298a
M1ddlzh <zyinni@126.com>
2020-03-12 18:05:52 +0800

    Lyapunov fix bug2

--- Lyapunov/Calculate.py
+++ Lyapunov/Calculate.py
@@ -1,180 +1,19 @@
 import cvxpy as cp
 import numpy as np
 
+"""
+Windows 10: cvxpy 1.0.21 and Ubuntu 16.04: cvxpy 1.0.27 have tested.
+"""
 
 alpha1 = 0.5
 alpha2 = 0.5
 beta1 = 0.5
 beta2 = 0.5
-beta0 = 0
-epsilon = 0.05
-epsilon1 = 0.002
-beta10 = 1
-n = 2
-cpln2 = 0.6931471805599453      # ln2 = 0.6931471805599453
 
-def Pmin(rr1, rr2, delt, h1, h2, N):
-    pmi1 = (2**(rr1/delt)-1)*delt*N/h1
-    pmi2 = (2**(rr2/(1-delt))-1)*(1-delt)*N/h2
-    return pmi1,pmi2
-
-
-def TempP_EE(v, Q1, Q2, Z1, Z2, delt, eta, N, h11, h22, pmi1, pmi2, pma):
-    pp1 = (v*alpha1+2*Q1)*delt/(np.log(2)*(2*Z1+v*eta*beta1))-delt*N/h11   
-    pp2 = (1-delt)*((v*alpha2+2*Q2)/((2*Z2+v*eta*beta2)*np.log(2))-N/h22)
-    p11 = min(max(pp1, pmi1), pma)
-    p22 = min(max(pp2, pmi2), pma)
-    return p11, p22
-
-def obj_EE(v, delt, h11, h22, p1, p2, N, eta, Z1, Z2, Q1, Q2, Y1, Y2, rr1, rr2, phi1, phi2):
-    RR1 = delt*np.log2(1+h11*p1/(delt*N))
-    RR2 = (1-delt)*np.log2(1+h22*p2/((1-delt)*N))
-    Tempobj = v*(alpha1*RR1+alpha2*RR2-eta*beta1*p1-beta2*eta*p2)-2*(Z1*p1+Z2*p2+Q1*rr1+Q2*rr2-Y1*phi1-Y2*phi2)+2*(Q1*RR1+Q2*RR2)
-    return Tempobj,RR1,RR2
-
-    #####below cross-layer##
-def TempP(v, Q1, Q2, Z1, Z2, delt, N, h11, h22, pmi1, pmi2, pma):
-    pp1=delt*(2*Q1/((v*alpha1+2*Z1)*np.log(2))-N/h11)
-    pp2=(1-delt)*(2*Q2/((v*alpha2+2*Z2)*np.log(2))-N/h22)
-    p11 = min(max(pp1,pmi1), pma)
-    p22 = min(max(pp2,pmi2), pma)
-    return p11,p22
-
-def obj(v, delt, h11, h22, p1, p2, N, Z1, Z2, Q1, Q2, Y1, Y2, rr1, rr2, phi1, phi2):
-    RR1 = delt*np.log2(1+h11*p1/(delt*N))
-    RR2 = (1-delt)*np.log2(1+h22*p2/((1-delt)*N))
-    if RR1<rr1:
-        b1=0
-    else:b1=RR1-rr1
-    if RR2<rr2:
-        b2=0
-    else:b2=RR2-rr2
-    Tempobj = v * (alpha1 * p1 + alpha2 * p2 - beta1 * phi1 - beta2 * phi2) \
-              -2 * (Y1*phi1 +Y2*phi2- Z1 * p1 - Z2 * p2)-2*Q1*b1-2*Q2*b2
-    return Tempobj,RR1,RR2,b1,b2
-
-def crosslayer(r1, r2, h11, h12, h21, h22, N, Y1, Y2, Q1, Q2, Z1, Z2, V, pma,phi1,phi2):
-    # print('cr')
-    epsilon1=0.002
-    lcr = (2 ** r1 - 1) * (2 ** r2 - 1) * h12 * h21 / h11 / h22
-    lcr1 = N * (((2 ** r1) - 1) * h21 / h11 + lcr) / (h21 * (1 - lcr))
-    lcr2 = N * (((2 ** r2) - 1) * h12 / h22 + lcr) / (h12 * (1 - lcr))
-    n=2
-    p_max=pma*np.ones(n)
-    p_min=np.zeros(n)
-    # objcr = cp.Parameter()
-    # print('pnot1,pnot2',lcr1,lcr2)
-    if lcr<1 and lcr1 <= pma and lcr2 <= pma:
-
-        objcr = 10000
-        b1 = (2 ** r1 - 1) * N / h11
-        b2 = (2 ** r2 - 1) * N / h22
-        B = [-b1, -b2]
-
-
-        if r1>0:
-            pt1 = h11 * pma / (h12 * (2 ** r1 - 1)) - N / h12
-        else:pt1=pma
-        if r2>0:
-            pt2 = h22 * pma / (h21 * (2 ** r2 - 1)) - N / h21
-        else:pt2=pma
-        if 0<pt1 and pt1 < pma:
-            pk1 = pma
-            pk2 = pt1
-        elif 0<pt2 and pt2< pma:
-            pk1 = pt2
-            pk2 = pma
-        else:
-            pk1 = pma
-            pk2 = pma
-
-        I = 0
-        while True:  
-            I+=1
-            a12 = h12 * ((2 ** r1) - 1) / h11
-            a21 = h21 * ((2 ** r2) - 1) / h22
-            A = [[-1, a12], [a21, -1]]
-
-            p = cp.Variable(shape=(2,), nonneg=True)
-            f = (V* beta1 + 2 * Z1) * p[0] - V * beta10*phi1 - 2 * Y1*phi1 + 2 * Q1 * r1 + (V * beta2+ 2 * Z2) * p[1] - V \
-                * beta10*phi2 - 2 * Y2*phi2 + 2 * Q2 * r2 \
-                - 2 * Q1 * np.log2(cp.exp(1)) * cp.log(h11 * p[0] + h12 * p[1] + N) - 2 * Q2 * np.log2(cp.exp(1)) * cp.log(
-                h22 * p[1] + h21 * p[0] + N)
-
-            g = -2 * Q1 *np.log2(h12 * pk2 + N) - 2 * Q2 * np.log2(h21 * pk1 + N)  ###
-            deltaG = [-2 * Q2 * h21 / ((h21 * pk1 + N) * cp.log(2)),
-                      -2 * Q1 * h12/ ((h12 * pk2 + N) * cp.log(2))]  ###
-            vectorP = [p[0] - pk1, p[1] - pk2] ###
-            # object1 = cp.Minimize(f - g - deltaG*vectorP)
-            object3 = cp.Minimize(f - g - deltaG[0]*vectorP[0]-deltaG[1]*vectorP[1])  ###
-            constr3 = [p_min <= p, p <= p_max, A *p-B<=0]
-            # constr1= [p_min <= p]
-            # constr2=[p <= p_max]
-            # constr3=[A * p + B <= p_min]
-            # prob = cp.Problem(object, constr1+constr2+constr3)
-            prob3 = cp.Problem(object3, constr3)
-            prob3.solve(solver='SCS')
-            if prob3.status == 'optimal' or prob3.status == 'optimal_inaccurate':
-                pp1 = max(p.value[0],0)
-                pp2 = max(p.value[1],0)
-                # print('pp1,pp2', pp1, pp2)
-                if phi1!=0 and pp1==0:
-                    pp1 = 0
-                    pp2 = 0
-                    objcr= 1000
-                    Rk1 = 0
-                    Rk2 = 0
-
-                    break
-                if phi2!=0 and pp2==0:
-                    pp1 = 0
-                    pp2 = 0
-                    objcr = 1000
-                    Rk1 = 0
-                    Rk2 = 0
-                    break
-                crk1=h11 * pp1 / (N + h12 * pp2)
-                crk2= h22 * pp2 / (N + h21 * pp1)
-                # print('crpp1,crpp2',pp1,pp2)
-                # if crk1>=0 and crk2>=0:
-                Rk1 = np.log2(1 + crk1)
-                Rk2 = np.log2(1 +crk2)
-                # else:
-                #     Rk2=0
-                #     Rk1=0
-                # print('crRk1,crRk2', Rk1, Rk2)
-                # print('p1,pk1',p.value[0],pk1)
-                # print('p2,pk2', p.value[1], pk2)
-
-                if abs(objcr-object3.value)<=epsilon1:
-                    objcr=object3.value
-                    break
-                elif I>=20:
-                    objcr = object3.value
-                    break
-                else:
-                    objcr = object3.value
-                    pk1 = pp1
-                    pk2 = pp2
-            else:
-                pp1 = 0
-                pp2 = 0
-                objcr= 1000
-                Rk1 = 0
-                Rk2 = 0
-                break
-    else:
-        pp1 = 0
-        pp2 = 0
-        objcr = 1000
-        Rk1 = 0
-        Rk2 = 0
-
-    return objcr, pp1, pp2, Rk1, Rk2
-
+cpln2 = 0.6931471805599453              # ln2 = 0.6931471805599453
 
 def NOA(h11, h12, h21, h22, v, QQ1, QQ2, r1, r2, N, phii1, phii2, pma, eta, YY1, YY2):
-    epsilon1 = 0.01     # iteration threshold
+    epsilon = 0.01                      # iteration breaking threshold
 
     k = (2 ** r1 - 1) * (2 ** r2 - 1) * h12 * h21 / (h11 * h22)     
     pnot1 = N * ((2 ** r1 - 1) * h21 / h11 + k) / (h21 * (1 - k))       # minimum power required
@@ -183,7 +22,7 @@ def NOA(h11, h12, h21, h22, v, QQ1, QQ2, r1, r2, N, phii1, phii2, pma, eta, YY1,
     p_min = np.array([[0], [0]])        # 2 × 1
 
     if k < 1 and pnot1 <= pma and pnot2 <= pma:
-        I = 0               # iteration num
+        I = 0                           # iteration num
         objNOA = -10000
 
         # initialize pk1, pk2
@@ -209,8 +48,6 @@ def NOA(h11, h12, h21, h22, v, QQ1, QQ2, r1, r2, N, phii1, phii2, pma, eta, YY1,
 
         while True:
             I += 1
-            A = np.array([[-1, h12 * (2 ** r1 - 1) / h11], [h21 * (2 ** r2 - 1) / h22, -1]])    # 2 × 2
-            B = np.array([[-(2 ** r1 - 1) * N / h11], [-(2 ** r2 - 1) * N / h22]])              # 2 × 1
             p = cp.Variable(shape=(2, 1), nonneg=True)                                          # 2 × 1, np.dot(A, p) - B
             # if use "f = (...) / cp.log(2)", will raise error "Problem does not follow DCP rules."
             f = (v * alpha1 + 2 * QQ1) * cp.log(N + h11 * p[0][0] + h12 * p[1][0]) / cpln2 + \
@@ -219,41 +56,44 @@ def NOA(h11, h12, h21, h22, v, QQ1, QQ2, r1, r2, N, phii1, phii2, pma, eta, YY1,
                 2 * (QQ1 * r1 - phii1 * YY1 + QQ2 * r2 - phii2 * YY2)                           # u_i, v_i = 1, 1, omit
             y1 = N + h12 * pk2
             y2 = N + h21 * pk1
-            g = (v * alpha1 + 2 * QQ1) * cp.log(y1) / cp.log(2) + (v * alpha2 + 2 * QQ2) * cp.log(y2) / cp.log(2)
+            g = (v * alpha1 + 2 * QQ1) * cp.log(y1) / cpln2 + (v * alpha2 + 2 * QQ2) * cp.log(y2) / cpln2
             
             vectorP = np.array([p[0][0] - pk1, p[1][0] - pk2])
-            deltaG = np.array([(v*alpha2 + 2 * QQ2) * h21 / ((N + h21 * pk1) * cp.log(2)),
-                               (v*alpha1 + 2 * QQ1) * h12 / ((N + h12 * pk2) * cp.log(2))])
+            deltaG = np.array([(v*alpha2 + 2 * QQ2) * h21 / ((N + h21 * pk1) * cpln2),
+                               (v*alpha1 + 2 * QQ1) * h12 / ((N + h12 * pk2) * cpln2)])
+
+            A = np.array([[-1, h12 * (2 ** r1 - 1) / h11], [h21 * (2 ** r2 - 1) / h22, -1]])    # 2 × 2
+            B = np.array([[-(2 ** r1 - 1) * N / h11], [-(2 ** r2 - 1) * N / h22]])              # 2 × 1                   
            
-            objfunc4 = cp.Maximize(f - g - deltaG[0] * vectorP[0] - deltaG[1] * vectorP[1])  
-            constr4 = [p_min <= p, p <= p_max, (A * p - B) <= 0 ]
-            prob4 = cp.Problem(objfunc4, constr4)
-            # print(prob4)
-            prob4.solve(solver = cp.SCS)
-            if prob4.status == 'optimal' or prob4.status == 'optimal_inaccurate':
+            objfunc = cp.Maximize(f - g - deltaG[0] * vectorP[0] - deltaG[1] * vectorP[1])  
+            constr = [p_min <= p, p <= p_max, (A * p - B) <= 0 ]
+            prob = cp.Problem(objfunc, constr)
+            # print(prob)
+            prob.solve(solver = cp.SCS)
+            if prob.status == 'optimal' or prob.status == 'optimal_inaccurate':
                 pp1 = max(p.value[0][0], 0)
                 pp2 = max(p.value[1][0], 0)
-                k1 = h11 * pp1 / (N + h12 * pp2)    # SINR
+                k1 = h11 * pp1 / (N + h12 * pp2)        # SINR
                 k2 = h22 * pp2 / (N + h21 * pp1)
                 if k1 >= 0 and k2 >= 0:
-                    Rk1 = np.log2(1 + k1)
+                    Rk1 = np.log2(1 + k1)               # channel capacity, bps/Hz
                     Rk2 = np.log2(1 + k2)
                 else:
                     pp1 = 0
                     pp2 = 0
-                    objNOA = prob4.value
+                    objNOA = prob.value
                     Rk1 = 0
                     Rk2 = 0
-                if abs(objNOA - objfunc4.value) <= epsilon1:
-                    objNOA = objfunc4.value
+                if abs(objNOA - objfunc.value) <= epsilon:
+                    objNOA = objfunc.value
                     break
                 elif I >= 20:
-                    objNOA = objfunc4.value
+                    objNOA = objfunc.value
                     break
                 else:
                     pk1 = pp1
                     pk2 = pp2
-                    objNOA = objfunc4.value
+                    objNOA = objfunc.value
             else:
                 pp1 = 0
                 pp2 = 0
--- Lyapunov/TEST.py
+++ Lyapunov/TEST.py
@@ -1,30 +1,36 @@
+r"""
+Original Paper: D. Lan, C. Wang, P. Wang, F. Liu and G. Min, "Transmission Design for Energy-Efficient 
+Vehicular Networks with Multiple Delay-Limited Applications," 2019 IEEE Global Communications Conference (GLOBECOM), 
+Waikoloa, HI, USA, 2019, pp. 1-6.
+doi: 10.1109/GLOBECOM38437.2019.9014246
+keywords: {Optimization;Sensors;Reliability;Delays;Power demand;Encoding;Linear programming},
+URL: http://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=9014246&isnumber=9013108
+"""
+
+
 import os
 import datetime
 import numpy as np
-import matplotlib.pyplot as plt
 from tqdm import tqdm
-from Calculate import Pmin, TempP, TempP_EE, obj, obj_EE, NOA, crosslayer
+from Calculate import NOA
 
 
 np.random.seed(0)
 
-global R1, R2, R_1, R_2, Rvv_1, fr_r1, fr_r2
+actions = [[1, 1], [1, 0], [0, 1], [0, 0]]
 
-phi = [[1, 1], [1, 0], [0, 1], [0, 0]]  # 4 actions
+phi_th = 0.9                            # ty1 transmission success shreshold
 
-phi_th = 0.9        # ty1 transmission success shreshold
-
-alpha1 = 0.5        # weight of R and P of two pairs of vehicles
+alpha1 = 0.5                            # weight of R and P of two pairs of vehicles
 alpha2 = 0.5
 beta1 = 0.5
 beta2 = 0.5
 
-dii = 10            # distance, m
+dii = 10                                # distance, metre
 d21 = 100
 d12 = 120
-N0 = 10**(-15)          # W/Hz
-band_width = 1e5        # 100 kHz
-N0 = N0 * band_width    # Watt
+N0 = 10**(-15)                          # W/Hz
+band_width = 100                        # 100 kHz
 PLii = - (103.4 + 24.2 * np.log10(dii / 1000))
 PL12 = - (103.4 + 24.2 * np.log10(d21 / 1000))
 PL21 = - (103.4 + 24.2 * np.log10(d12 / 1000))
@@ -32,159 +38,117 @@ sigmaii = (10**(PLii / 10))
 sigma12 = (10**(PL12 / 10))    
 sigma21 = (10**(PL21 / 10))      
 
-T = 1000                    # time slot
+T = 1000                                # time slot
 
-VV = np.arange(2, 7, 2)     # weight of energy efficiency
+ALL_V = np.arange(20, 24, 5)            # weight of energy efficiency
 
-r = [0, 200]                # ty1 transmission rate
-lamda = 500                 # ty2 generate rate
-pmax_dbm = np.arange(23, 24, 3)     # power, dBm
-pnum = len(pmax_dbm)
+r = [0, 100 / band_width]               # ty1 transmission rate, bit/Hz
+lamda = 300 / band_width                # ty2 generate rate, bit/Hz
 
-EE = [[],[],[]]             #energy efficiency
-AvgsumPower = [[],[],[]]
-AvgsumRate = [[], [], []]
-Y = [[],[],[]]              # ty1 queue
-Q = [[],[],[]]              # ty2 queue
-REL = [[], [], []]          # reliability of ty1
+pmax_dbm = 23                           # power, dBm
+pmax = (10 ** (pmax_dbm / 10)) / 1000   # dBm -> W
 
-for vnum in range(len(VV)):
-    V = VV[vnum]
-    for a in range(pnum):
-        P_max = pmax_dbm[a]
-        pmax = (10 ** (P_max / 10)) / 1000      # dBm -> W
-        Y1 = 0.0
-        Y2 = 0.0
-        Q1 = 0.0 
-        Q2 = 0.0
-        eta = 0.0       # ee
-        Rv1 = 0.0       # sum rate
-        Pv1 = 0.0       # sum power
-        sumQ = 0.0
-        sumY = 0.0
-        rel = 0.0
-        for t in tqdm(range(T), ncols = 60):
-            h11 = np.random.gamma(2, sigmaii / 2)  
-            h22 = np.random.gamma(2, sigmaii / 2)
-            h12 = np.random.gamma(2, sigma12 / 2)
-            h21 = np.random.gamma(2, sigma21 / 2)
-            # print(h11, h22, h12, h21)
+EE = []                                 # energy efficiency
+AvgsumPower = []
+AvgsumRate = []
+Y = []                                  # ty1 queue
+Q = []                                  # ty2 queue
+RELI = []                               # reliability of ty1
 
-            a1 = np.random.poisson(lam = lamda)     # ty2 data
-            a2 = np.random.poisson(lam = lamda)
+for vnum in range(len(ALL_V)):
+    V = ALL_V[vnum]                     
+    Y1 = 0.0
+    Y2 = 0.0
+    Q1 = 0.0 
+    Q2 = 0.0
+    eta = 0.0                           # ee
+    Rv = 0.0                            # sum rate
+    Pv = 0.0                            # sum power
+    sumQ = 0.0
+    sumY = 0.0
+    reli = 0.0
+    for t in tqdm(range(T), ncols = 60):
+        h11 = np.random.gamma(2, sigmaii / 2)  
+        h22 = np.random.gamma(2, sigmaii / 2)
+        h12 = np.random.gamma(2, sigma12 / 2)
+        h21 = np.random.gamma(2, sigma21 / 2)
 
-            OBJ = -1000         # a small initial objective
-            R1 = 0
-            R2 = 0
-            P1 = 0
-            P2 = 0
-            b1_ee = 0           # ty2 transmission rate
-            b2_ee = 0
-            PHI1 = 0            # the best of the four actions
-            PHI2 = 0
+        a1 = np.random.poisson(lam = lamda)     # ty2 data
+        a2 = np.random.poisson(lam = lamda)
 
-            for i in range(4):  
-                phi1 = phi[i][0]    # transfer ty1 or not
-                phi2 = phi[i][1]
-                tempr1 = r[phi1]
-                tempr2 = r[phi2]
+        OBJ = -10000                            # a small initial objective
+        R1 = 0
+        R2 = 0
+        P1 = 0
+        P2 = 0
+        b1 = 0                                  # ty2 transmission rate
+        b2 = 0
+        PHI1 = 0                                # the best of the four actions
+        PHI2 = 0
 
-                tempobj, tp1, tp2, tempR1, tempR2 = \
-                    NOA(h11, h12, h21, h22, V, Q1, Q2, tempr1, tempr2, N0, phi1, phi2, pmax, eta, Y1, Y2)
+        for i in range(len(actions)):  
+            phi1 = actions[i][0]                # transfer ty1 or not
+            phi2 = actions[i][1]
+            tempr1 = r[phi1]
+            tempr2 = r[phi2]
 
-                if tempobj > OBJ:
-                    OBJ = tempobj
-                    P1 = tp1
-                    P2 = tp2
-                    PHI1 = phi1
-                    PHI2 = phi2
-                    R1 = tempR1
-                    R2 = tempR2
-                    if R1 > tempr1:
-                        b1_ee = R1 - tempr1
-                    else:
-                        b1_ee = 0
-                    if R2 > tempr2:
-                        b2_ee = R2 - tempr2
-                    else:
-                        b2_ee = 0
-            
-            sumQ += (Q1 + Q2)
-            sumY += (Y1 + Y2)
-            Y1 = max(Y1 - PHI1, 0) + phi_th     # ty1 queue
-            Y2 = max(Y2 - PHI2, 0) + phi_th
-            Q1 = max(Q1 - b1_ee, 0) + a1        # ty2 queue
-            Q2 = max(Q2 - b2_ee, 0) + a2
-            
-            Rv1 += (alpha1 * R1 + alpha2 * R2)
-            Pv1 += (beta1 * P1 + beta2 * P2)
+            tempobj, temp1, temp2, tempR1, tempR2 = \
+                NOA(h11, h12, h21, h22, V, Q1, Q2, tempr1, tempr2, N0, phi1, phi2, pmax, eta, Y1, Y2)
 
-            if Pv1 == 0.0:
-                eta = 0.0
-            else:
-                eta = Rv1 / Pv1
-            rel += (PHI1 + PHI2)    
+            if tempobj > OBJ:
+                OBJ = tempobj
+                P1 = temp1
+                P2 = temp2
+                PHI1 = phi1
+                PHI2 = phi2
+                R1 = tempR1
+                R2 = tempR2
+                if R1 > tempr1:
+                    b1 = R1 - tempr1
+                else:
+                    b1 = 0
+                if R2 > tempr2:
+                    b2 = R2 - tempr2
+                else:
+                    b2 = 0
+        
+        sumQ += (Q1 + Q2)
+        sumY += (Y1 + Y2)
+        Y1 = max(Y1 - PHI1, 0) + phi_th     # ty1 queue
+        Y2 = max(Y2 - PHI2, 0) + phi_th
+        Q1 = max(Q1 - b1, 0) + a1           # ty2 queue
+        Q2 = max(Q2 - b2, 0) + a2
+        
+        Rv += (alpha1 * R1 + alpha2 * R2)
+        Pv += (beta1 * P1 + beta2 * P2)
 
-        if Pv1 == 0.0:
-            EE[vnum].append(0.0)
+        if Pv == 0.0:
+            eta = 0.0
         else:
-            EE[vnum].append(round((Rv1 / Pv1), 3))
+            eta = Rv / Pv
+        reli += (PHI1 + PHI2)  
 
-        AvgsumPower[vnum].append(((Pv1) / T))
-        Q[vnum].append(round((sumQ) / T, 3))
-        REL[vnum].append(round(rel / (2 * T), 3))
-        AvgsumRate[vnum].append(round(Rv1 / T, 3))
-        Y[vnum].append(round((sumY) / T, 3))
+    if Pv == 0.0:
+        EE.append(0.0)
+    else:
+        EE.append(round(Rv / Pv, 4))
+    AvgsumPower.append(round(Pv / T, 4))
+    AvgsumRate.append(round(Rv / T, 4))
+    Y.append(round(sumY / T, 4))
+    Q.append(round(sumQ / T, 4))
+    RELI.append(round(reli / (2 * T), 4))  
 
-path = './fig_result/'
+print('EE: ', EE, '\n', 'Y: ', Y, '\n', 'Q: ', Q, '\n', 'RELI: ', RELI, sep='')
+
+path = './Lyapunov_results/'
 if not os.path.exists(path):
     os.makedirs(path)
-
-fig1 = plt.figure("0420 23 36Achievable energy efficiency") 
-PE1 = plt.semilogy(pmax_dbm, EE[0], color='blue', marker='o', label='EE-based design, V=' + str(VV[0]))
-PE2 = plt.semilogy(pmax_dbm, EE[1], color='orange', marker='*', label='EE-based design, V=' + str(VV[1]))
-PE3 = plt.semilogy(pmax_dbm, EE[2], color='red', marker='o', label='EE-based design, V=' + str(VV[2]))
-plt.xlabel("pmax_dbm")
-plt.ylabel("Energy Efficiency")
-plt.legend(loc='upper right')
-plt.savefig(path + "Energy_Efficiency_" + datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S'), dpi = 401)
-
-fig2 = plt.figure("Reliability of type-1 messages")
-PR1 = plt.plot(pmax_dbm, REL[0], color='blue', linestyle='-', label='EE-based design, V=' + str(VV[0]))
-PR2 = plt.plot(pmax_dbm, REL[1], color='orange', linestyle='-.', label='EE-based design, V=' + str(VV[1]))
-PR3 = plt.plot(pmax_dbm, REL[2], color='red', linestyle='-', label='EE-based design, V=' + str(VV[2]))
-plt.xlabel("pmax_dbm")
-plt.ylabel("Reliability")
-plt.legend(loc='upper right')
-plt.savefig(path + "Reliability_" + datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S'), dpi = 401)
-
-fig3= plt.figure("queue length of type-2 messages")
-PQ1 = plt.plot(pmax_dbm, Q[0], color='blue', linestyle='-.', label='EE-based design, V=' + str(VV[0]))
-PQ2 = plt.plot(pmax_dbm, Q[1], color='orange', linestyle='-.', label='EE-based design, pmax=' + str(VV[1]))
-PQ3 = plt.plot(pmax_dbm, Q[2], color='red', linestyle='-.', label='EE-based design, pmax=' + str(VV[2]))
-plt.xlabel("pmax_dbm")
-plt.ylabel("Queue length")
-plt.legend(loc='upper right')
-plt.savefig(path + "Queue_length_" + datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S'), dpi = 401)
-
-fig4 = plt.figure("AvgsumRate")
-PRA1 = plt.plot(pmax_dbm, AvgsumRate[0], color='blue', linestyle='-', label='EE-based design,V=' + str(VV[0]))
-PRA2 = plt.plot(pmax_dbm, AvgsumRate[1], color='orange', linestyle='-', label='EE-based design,V=' + str(VV[1]))
-PRA3 = plt.plot(pmax_dbm, AvgsumRate[2], color='red', linestyle='-', label='EE-based design,V=' + str(VV[2]))
-plt.xlabel("pmax_dbm")
-plt.ylabel("AvgsumRate")
-plt.legend(loc='upper right')
-plt.savefig(path + "AvgsumRate_" + datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S'), dpi = 401)
-
-fig5 = plt.figure("AvgsumPower")
-VP1=plt.plot(pmax_dbm, AvgsumPower[0], color='blue', linestyle='--', label='EE-based design,V=' + str(VV[0]))
-VP2=plt.plot(pmax_dbm, AvgsumPower[1], color='orange', linestyle='--', label='EE-based design,V=' + str(VV[1]))
-VP3=plt.plot(pmax_dbm, AvgsumPower[2], color='red', linestyle='-', label='EE-based design,V=' + str(VV[2]))
-plt.xlabel('pmax_dbm')
-plt.ylabel('AvgsumPower')
-plt.legend(loc='upper right')
-plt.savefig(path + "AvgsumPower_" + datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S'), dpi = 401)
-
-plt.show()
-
+with open(path + datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + '.txt', 'w+') as f:
+    f.write('ALL_V: ' + str(ALL_V) + '\n')
+    f.write('Energy Efficiency:'); f.write(str(EE) + '\n')
+    f.write('Y:'); f.write(str(Y) + '\n')
+    f.write('Q:'); f.write(str(Q) + '\n')
+    f.write('RELI:'); f.write(str(RELI) + '\n')
+    f.write('AvgsumPower:'); f.write(str(AvgsumPower) + '\n')
+    f.write('AvgsumRate:'); f.write(str(AvgsumRate) + '\n')
 
